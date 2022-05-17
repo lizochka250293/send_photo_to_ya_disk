c9fb1bd58d9349b2dea1295ce05b58bfe22bbbfa
vera <lerika250293@mail.ru>
2022-05-17 10:13:11 +0300

    dont work exsist

--- main.py
+++ main.py
@@ -13,25 +13,25 @@ token_list = []
 def send_welcome(message):
     bot.send_message(message.chat.id, 'Получи и отправь токен https://oauth.yandex.ru/client/new')
 
-
-
 @bot.message_handler(content_types=['text'])
 def token(message):
     token = message.text
     token_list.append(token)
-    print(token_list)
     bot.send_message(message.chat.id, 'Отправьте файл для загрузки')
-
 
 @bot.message_handler(content_types=['photo', 'document'])
 def upload_step(message):
     date = datetime.strftime(datetime.now(), "%d.%m.%Y-%H.%M.%S")
-    if message.content_type == "document":
+    if message.text == '/break':
+        bot.send_message(message.chat.id, 'Пожалуй, я сохраню это')
+        delete()
+        return
+
+    elif message.content_type == "document":
         if message.document.file_id not in document_list:
             file_info = bot.get_file(message.document.file_id)
             downloaded_file = bot.download_file(file_info.file_path)
             document_list.append(message.document.file_id)
-            print(document_list)
             src = 'files/' + file_info.file_path
             with open(src, 'wb') as new_file:
                 new_file.write(downloaded_file)
@@ -48,7 +48,8 @@ def upload_step(message):
                         bot.reply_to(message, "Проверь токен")
                     else:
                         y.upload(f"{src}", f"/telegram/{file_to_path}")
-                        bot.reply_to(message, "Пожалуй, я сохраню это")
+                        sent = bot.reply_to(message, '/break, если загрузка завершена')
+                        bot.register_next_step_handler(sent, upload_step)
                 except:
                     try:
                         y.check_token()
@@ -56,14 +57,14 @@ def upload_step(message):
                         bot.reply_to(message, e)
                     else:
                         y.upload(f"{src}", f"/telegram/{file_to_path}")
-                        bot.reply_to(message, "Пожалуй, я сохраню это")
+                        sent = bot.reply_to(message, '/break, если загрузка завершена')
+                        bot.register_next_step_handler(sent, upload_step)
 
     elif message.content_type == "photo":
         if message.photo[-1].file_id not in photo_list:
             file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
             downloaded_file = bot.download_file(file_info.file_path)
             photo_list.append(message.photo[-1].file_id)
-            print(photo_list)
             src = 'files/' + file_info.file_path
             with open(src, 'wb') as new_file:
                 new_file.write(downloaded_file)
@@ -80,6 +81,8 @@ def upload_step(message):
                         bot.reply_to(message, e)
                     else:
                         y.upload(f"{src}", f"/telegram/{file_to_path}")
+                        sent = bot.reply_to(message, '/break, если загрузка завершена')
+                        bot.register_next_step_handler(sent, upload_step)
                 except:
                     try:
                         y.check_token()
@@ -87,11 +90,14 @@ def upload_step(message):
                         bot.reply_to(message, "Проверь токен")
                     else:
                         y.upload(f"{src}", f"/telegram/{file_to_path}")
-                        bot.reply_to(message, "Пожалуй, я сохраню это")
+                        sent = bot.reply_to(message, '/break, если загрузка завершена')
+                        bot.register_next_step_handler(sent, upload_step)
 
 
-bot.polling()
+
+
 def delete():
+
     folder = 'C:/Users/bt030/PycharmProjects/send_photo_yandex_disk/files/documents'
     for filename in os.listdir(folder):
         file_path = os.path.join(folder, filename)
@@ -112,8 +118,7 @@ def delete():
                 shutil.rmtree(file_path)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
-delete()
-# y = yadisk.YaDisk(token="AQAAAAACqMGvAAfkuMxxMfH-C0uPp9O6eNnSQ54")
-# print(y.check_token())
-# # y.mkdir("/test") # Создать папку
-# y.upload("text.txt", "/test/file1.txt")
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
+    delete()
